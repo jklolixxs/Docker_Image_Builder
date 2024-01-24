4d858f82d043c352e0c80da0187327f2a8732821
jklolixxs <69113033+jklolixxs@users.noreply.github.com>
2024-01-25 03:26:43 +0800

    Update build.sh

--- sing-box/build.sh
+++ sing-box/build.sh
@@ -1,55 +1,70 @@
-# ❗❗❗如下操作均为Linux系统中的命令行操作❗❗❗
+#!/bin/bash
+# 主目录
+local_path="/opt/Docker_Image_Builder/sing-box"
+# Dockerfile路径
+dockerfile_path="/opt/Docker_Image_Builder/sing-box/Dockerfile"
+# 存储版本号的文件路径
+version_file="/opt/Docker_Image_Builder/sing-box/version_file.txt"
+# Docker Hub的用户名
+username="用户名"
+# Docker Hub中的仓库名
+repositories="仓库名"
 
-## 1.安装Docker 与 必备程序
-#### 使用Dcker官方一键安装脚本  
-```
-curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
-```
-#### （如果您之前有安装Docker，则无需再次执行，略过此条向下继续查看）
+# 读取本地储存的main_next版本号与dev_next版本号
+main_next_local_tag=$(awk '/main-next/{print $NF; exit}' $version_file)
+dev_next_local_tag=$(awk '/dev-next/{print $NF; exit}' $version_file)
+# 获取最新的main_next版本号与dev_next版本号（使用awk与jq两种方式，任选自己喜欢的方式，默认使用awk）
+read_tags=$(wget -qO- --tries=3 "https://api.github.com/repos/SagerNet/sing-box/releases" | awk -F '"' '/tag_name/{print $(NF-1)}')
+# read_tags=$(wget -qO- --tries=3 "https://api.github.com/repos/SagerNet/sing-box/releases" | jq -r '.[].tag_name')
+main_next_tag=$(grep -vm1 '-' <<<"$read_tags")
+dev_next_tag=$(grep -m1 '-' <<<"$read_tags")
 
-## 2.执行编译前的必要命令
-```
-docker buildx create --use  
-docker buildx inspect --bootstrap  
-docker login
-```
-#### 这两个命令通常用于设置和准备Docker Buildx，以便能够使用其功能构建适用于多个CPU架构和操作系统的容器镜像  
-#### （如果您之前有执行这两条命令，则无需再次执行，略过此条向下继续查看）
+# 检查是否成功获取到最新的版本号
+if [ -z "$main_next_tag" ] || [ -z "$dev_next_tag" ]; then
+    echo "无法获取最新的main-next版本号"
+    echo "1.请检查与GitHub之间的网络连接是否通畅"
+    echo "2.如短时间内读取版本号过多，可能会出发GitHub限制，请最短每分钟一次"
+    exit 1
+fi
 
-## 3.拉取仓库
-* 方法一：使用git拉取  
-```
-git clone https://github.com/jklolixxs/Docker_Image_Builder.git /opt/Docker_Image_Builder
-```
-* 方法二：下载压缩包自行解压
-  > 如果您不知道在哪里下载仓库压缩包，可以 **[点击此处一键下载](https://codeload.github.com/jklolixxs/Docker_Image_Builder/zip/refs/heads/main)**
+# 检查main_next版本号是否有变动
+if [ "$main_next_local_tag" != "$main_next_tag" ]; then
+    # 更新main_next版本号
+    sed -i "s/\(main-next.*:\).*/\1 $main_next_tag/" $version_file
 
-## 4.检查路径
-检查 **sing-box** 文件夹是否存在于 /opt/Docker_Image_Builder/ 路径中  
+    # 更新临时Dockerfile中的版本号
+    mkdir -p $local_path/tmp/main-next/ && cp $dockerfile_path $local_path/tmp/main-next/Dockerfile
+    sed -i "s/git clone -b [^[:space:]]* --single-branch/git clone -b $main_next_tag --single-branch/g" "$local_path/tmp/main-next/Dockerfile"
 
-## 5.更改细节
-用您喜欢的文本编辑器，编辑 `/opt/Docker_Image_Builder/sing-box/build.sh` 文件中的的内容  
-找到9 11行，将 `用户名`修改为您的Docker Hub用户名，将 `仓库名` 修改为您像推送到的Docker Hub仓库的名字，如果没有这个命名的仓库也没关系，会自动创建然后推送
+    # 执行 Docker 命令
+    docker buildx build --platform linux/i386,linux/amd64,linux/arm/v7,linux/arm64 --push -t $username/$repositories:$main_next_tag -f "$local_path/tmp/main-next/Dockerfile" .
+    docker buildx build --platform linux/i386,linux/amd64,linux/arm/v7,linux/arm64 --push -t $username/$repositories:latest -f "$local_path/tmp/main-next/Dockerfile" .
+    echo "成功编译，当前main-next版本号为$main_next_tag"
 
-## 6.提权
-给予一键编译脚本权限  
-```
-chmod +x /opt/Docker_Image_Builder/sing-box/build.sh
-```
+    # 移除临时Dockerfile
+    rm -rf $version_file/tmp/
+else
+    # 如果main_next版本号没有变动，则输出提示信息
+    echo "main_next版本号没有变动，无需执行更新 main_next Docker 命令。"
+fi
 
-## 7.开始编译
-运行命令，脚本会自动开始查询最新版本号，并开始，编译成功后会自动推送至对应仓库  
-```
-/opt/Docker_Image_Builder/sing-box/build.sh
-```
+# 检查dev_next版本号是否有变动
+if [ "$dev_next_local_tag" != "$dev_next_tag" ]; then
+    # 更新dev_next版本号
+    sed -i "s/\(dev-next.*:\).*/\1 $dev_next_tag/" $version_file
 
-## 8.关于全自动构建推送
-使用Linux自带的 **crontab** 来定时执行编译脚本  
-使用指令 `crontab -e -u root` 进入，在最后一行添加
-```
-* */1 * * * /opt/Docker_Image_Builder/sing-box/build.sh
-```
-保存并退出后，全自动构建脚本会每小时检查一次版本号，如果有变动，则会自动在后台进入构建并推送（不宜设置过短，容易触发GitHub限制，导致版本号查询失败）
->`crontab`: 用于编辑、查看或删除用户的 crontab 文件。  
->`-e`: 表示编辑 crontab 文件。  
->`-u root`: 指定要编辑的用户。在这里，root 是目标用户。  
+    # 更新临时Dockerfile中的版本号
+    mkdir -p $local_path/tmp/dev-next/ && cp $dockerfile_path $local_path/tmp/dev-next/Dockerfile
+    sed -i "s/git clone -b [^[:space:]]* --single-branch/git clone -b $dev_next_tag --single-branch/g" "$local_path/tmp/dev-next/Dockerfile"
+
+    # 执行 Docker 命令
+    docker buildx build --platform linux/i386,linux/amd64,linux/arm/v7,linux/arm64 --push -t $username/$repositories:$dev_next_tag -f "$local_path/tmp/dev-next/Dockerfile" .
+    docker buildx build --platform linux/i386,linux/amd64,linux/arm/v7,linux/arm64 --push -t $username/$repositories:dev-next -f "$local_path/tmp/dev-next/Dockerfile" .
+    echo "成功编译，当前dev-next版本号为$dev_next_tag"
+
+    # 移除临时Dockerfile
+    rm -rf $version_file/tmp/dev-next/Dockerfile
+else
+    # 如果dev_next版本号没有变动，则输出提示信息
+    echo "dev_next版本号没有变动，无需执行更新 dev_next Docker 命令。"
+fi
